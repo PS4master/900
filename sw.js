d6cdd7281d00832be6682764a72c779c1134beee
mgdot-jailbreak <ktarshia4@gmail.com>
2025-11-28 12:15:26 +0330

    Update sw.js

--- sw.js
+++ sw.js
@@ -8,7 +8,7 @@ const FILES = [
   "no usb.webp",
   "karo.webp",
   "Gemini_Generated_Image_j8sn1j8sn1j8sn1j.webp",
-  "ps4-luminous-wave-0xd36pumgigbl01x.jpg"
+  "ps4_wave_purple_compressed.jpg"
 ];
 
 // -------------------------
@@ -64,5 +64,9 @@ self.addEventListener("fetch", event => {
     })
   );
 });
+
+
+
+
 
 
